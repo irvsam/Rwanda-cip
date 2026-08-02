702df5690ac3a3e91435ad4b6e34f17524ad4b54
Sammy Irving <samantha.irving@stud.unilu.ch>
2026-08-02 11:18:17 +0200

    tweaking some stuff with luc intensity

--- scripts/03_build_luc.R
+++ scripts/03_build_luc.R
@@ -15,21 +15,48 @@ sas_b <- read_dta(file.path(data_path, "SAS 2024/Season B/Rwa_raw_SeasonB2024_Sc
 sas_c <- read_dta(file.path(data_path, "SAS 2024/Season C/Rwa_raw_SeasonC2024_Screening.dta")) %>% mutate(season = "C")
 
 
+# Look at all the labels to see which would be useful
+colnames(sas_a)
+colnames(sas_b)
+colnames(sas_c)
+View(labelled::look_for(sas_a))
+View(labelled::look_for(sas_b))
+View(labelled::look_for(sas_c))
+
+# clean up each one and just keep the following variables - segment id, s1q13, s2q1, s2q13, s2q7, s2q6, s2q12, s1q2, plotsize, plot weight
+clean_sas <- function(df) {
+  df %>%
+    select(Segment_ID,s1q1, s1q2, s1q13, s2q1, s2q6, s2q7, s2q12, Plot_size_ha, plot_weight)
+}
+
+clean_sas(sas_a) -> sas_a
+clean_sas(sas_b) -> sas_b
+clean_sas(sas_c) -> sas_c
+
+
 
 process_sas_season <- function(df, season_label) {
   # Filter for agricultural plots with a valid LUC response, group by district, and calculate total and LUC area estimates
+  
   df %>%
-    filter(is.na(s2q7), !is.na(s2q12)) %>%   # agricultural plots with a valid LUC response
-    group_by(s1q2) %>%                        # district
+    # first filter for agricultural plots which would be s2q76 = 96
+    filter(s2q7 == 96) %>%
+    filter(!is.na(s2q12)) %>%   #  valid LUC response
+    group_by(s1q2) %>%          # group by district
+    
+    
     summarise(
       total_ha_est = sum(Plot_size_ha * plot_weight, na.rm = TRUE),
       luc_ha_est   = sum((Plot_size_ha * plot_weight)[as.numeric(s2q12) == 1], na.rm = TRUE)
     ) %>%
+    
     mutate(
       season = season_label,
       seasonal_intensity = (luc_ha_est / total_ha_est) * 100
     )
 }
+
+
 # Combine the processed data from all three seasons and calculate the average LUC intensity per district
 dist_luc <- bind_rows(
   process_sas_season(sas_a, "A"),
@@ -50,4 +77,44 @@ saveRDS(dist_luc, file.path(processed_path, "dist_luc.rds"))
 # District map for visual representation later on
 rwa_map <- gadm(country = "RWA", level = 2, path = tempdir()) %>% st_as_sf()
 saveRDS(rwa_map, file.path(processed_path, "rwa_map.rds"))
+
+# -------------------------------------------------------------------
+
+
+# Now for 2019
+
+# Read in the SAS 2019 Screening datasets for Seasons A, B, and C
+sas_a <- read_dta(file.path(data_path, "older SAS/SAS 2019/rwa-sas-seasonA_Screening_Antierosion_land consolidation.dta")) %>% mutate(season = "A")
+sas_b <- read_dta(file.path(data_path, "older SAS/SAS 2019/rwa-sas-seasonB_Screening_Antierosion_land consolidation.dta")) %>% mutate(season = "B")
+sas_c <- read_dta(file.path(data_path, "older SAS/SAS 2019/rwa-sas-seasonC_Screening_Antierosion_land consolidation.dta")) %>% mutate(season = "C")
+
+
+# Look at the columns to see which would be useful
+colnames(sas_a)
+colnames(sas_b)
+colnames(sas_c)
+View(labelled::look_for(sas_a))
+View(labelled::look_for(sas_b))
+View(labelled::look_for(sas_c))
+
+sas_a_crops <- read_dta(file.path(data_path, "older SAS/SAS 2019/rwa-sas-seasonA-Screening_crops.dta")) %>% mutate(season = "A")
+sas_b_crops <- read_dta(file.path(data_path, "older SAS/SAS 2019/rwa-sas-seasonB-Screening_crops.dta")) %>% mutate(season = "B")
+sas_c_crops <- read_dta(file.path(data_path, "older SAS/SAS 2019/rwa-sas-seasonC-Screening_crops.dta")) %>% mutate(season = "C")
+
+
+
+# Get the size of the plots from each crop dataset
+
+
+
+
+
+
+
+
+
+
+
+
+
 
