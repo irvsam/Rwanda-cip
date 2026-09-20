acab3fed571f25250708d62573564a048ede5a03
Sammy Irving <samantha.irving@stud.unilu.ch>
2026-09-20 17:32:38 +0200

    cleaning up

--- scripts/create_report.R
+++ scripts/create_report.R
@@ -1,8 +1,15 @@
 library(tinytex)
 
+main <- "main.tex"
+base <- tools::file_path_sans_ext(main)
 
-# setwd("report/Seminar_Paper")
-tinytex::pdflatex("main.tex")
-system2("biber", "main")
-tinytex::pdflatex("main.tex")
-tinytex::pdflatex("main.tex")
+tinytex::pdflatex(main, clean = FALSE)
+system2("biber", base)
+tinytex::pdflatex(main, clean = FALSE)
+tinytex::pdflatex(main, clean = FALSE)
+
+# clean up auxiliary files, keep only the PDF
+aux_ext <- c("aux", "bbl", "bcf", "blg", "log", "out", "toc",
+             "lof", "lot", "fls", "fdb_latexmk", "run.xml", "synctex.gz")
+aux_files <- unlist(lapply(aux_ext, function(e) list.files(pattern = paste0("\\.", e, "$"))))
+if (length(aux_files) > 0) file.remove(aux_files)
